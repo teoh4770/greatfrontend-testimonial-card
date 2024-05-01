7a1bcc5d1392775d9a0f7d5653d3c5ae171c3e0f
Chee Kian Teoh <teoh4770@mylaurier.ca>
2024-05-01 15:42:30 -0400

    💄style: adding more minor styling

--- src/components/testimonial-card.tsx
+++ src/components/testimonial-card.tsx
@@ -12,19 +12,21 @@ const TestimonialCard: React.FC<TestimonialCardProps> = ({
   content,
 }) => {
   return (
-    <article className="testimonialCard grid  max-w-[21.25rem] gap-4 rounded-lg bg-white p-6 shadow-[0_1px_3px_0px_rgba(0,0,0,0.1)]">
+    <article className="testimonialCard grid  max-w-[21.25rem] gap-4 rounded-lg border border-neutral-200 bg-white p-6 shadow-[0_1px_3px_0px_rgba(0,0,0,0.1)]">
       <header className="testimonialCard__header">
-        <figure className="testimonialCard__summary flex gap-4">
+        <figure className="testimonialCard__summary flex flex-wrap gap-4">
           <img
             className="testimonialCard__avatar aspect-square w-12 object-contain"
             src={profileThumbnail}
             alt={`${fullName}'s avatar`}
           />
           <figcaption>
-            <h3 className="testimonialCard__fullName text-lg font-semibold">
+            <h3 className="testimonialCard__fullName text-lg font-semibold text-neutral-900">
               {fullName}
             </h3>
-            <p className="testimonialCard__userName text-sm">@{userName}</p>
+            <p className="testimonialCard__userName text-sm text-neutral-600">
+              @{userName}
+            </p>
           </figcaption>
         </figure>
       </header>
@@ -32,7 +34,9 @@ const TestimonialCard: React.FC<TestimonialCardProps> = ({
         className="testimonialCard__content"
         aria-labelledby="testimonialContent"
       >
-        <p id="testimonialContent">{content}</p>
+        <p id="testimonialContent" className="text-neutral-600">
+          {content}
+        </p>
       </blockquote>
     </article>
   )
