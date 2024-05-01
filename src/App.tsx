4b751751960149649519c2b5d7f84f9befb1bca9
Chee Kian Teoh <teoh4770@mylaurier.ca>
2024-05-01 14:17:30 -0400

    :tada:setup: start a project and add config files

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <>
+      <div>
+        <div className="credits">
+          A challenge by
+          <a
+            href="https://www.greatfrontend.com/projects?ref=challenges"
+            target="_blank"
+          >
+            GreatFrontEnd Projects
+          </a>
+          . Built by {''}
+          <a href="https://www.greatfrontend.com/u/teoh4770" target="_blank">
+            Chee Kian Teoh
+          </a>
+          .
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
