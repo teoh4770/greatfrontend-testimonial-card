fdd6ca4447842e6df70efc3e900bbe672326711c
Chee Kian Teoh <teoh4770@mylaurier.ca>
2024-05-01 16:15:09 -0400

    style: truncate the long string

--- src/components/testimonial-card.tsx
+++ src/components/testimonial-card.tsx
@@ -12,7 +12,8 @@ const TestimonialCard: React.FC<TestimonialCardProps> = ({
   content,
 }) => {
   return (
-    <article className="testimonialCard grid  max-w-[21.25rem] gap-4 rounded-lg border border-neutral-200 bg-white p-6 shadow-[0_1px_3px_0px_rgba(0,0,0,0.1)]">
+    <article className="testimonialCard grid max-w-[21.25rem] gap-4 rounded-lg border border-neutral-200 bg-white p-6 shadow-[0_1px_3px_0px_rgba(0,0,0,0.1)]">
+      {/* testimonial header */}
       <header className="testimonialCard__header">
         <figure className="testimonialCard__summary flex flex-wrap gap-4">
           <img
@@ -21,7 +22,7 @@ const TestimonialCard: React.FC<TestimonialCardProps> = ({
             alt={`${fullName}'s avatar`}
           />
           <figcaption>
-            <h3 className="testimonialCard__fullName text-lg font-semibold text-neutral-900">
+            <h3 className="testimonialCard__fullName truncate text-lg font-semibold text-neutral-900">
               {fullName}
             </h3>
             <p className="testimonialCard__userName text-sm text-neutral-600">
@@ -30,6 +31,7 @@ const TestimonialCard: React.FC<TestimonialCardProps> = ({
           </figcaption>
         </figure>
       </header>
+      {/* testimonial content */}
       <blockquote
         className="testimonialCard__content"
         aria-labelledby="testimonialContent"
